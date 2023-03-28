38d35683589bf6aef515b49c519521aaa149e30b
nicollorens12 <nickskate23@gmail.com>
2023-03-28 13:02:25 +0200

    added LT Greedy

--- LT_graph.hh
+++ LT_graph.hh
@@ -3,7 +3,14 @@
 
 #include <vector>
 #include <iostream>
+#include <algorithm>
+
 using namespace std;
+
+struct node_info{
+    int node;
+    int degree;
+};
 
 class LTGraph{
 
@@ -17,6 +24,14 @@ class LTGraph{
         G = graph;
         r = r_a;
         S = S_a;
+    }
+
+    LTGraph(vector<vector<int> > graph, float r_a){
+        G = graph;
+        r = r_a;
+        int V = G.size();
+        S = vector<int> (V);
+        for(int i = 0; i < V; ++i) S[i] = i;
     }
 
     int procedure(){
@@ -65,6 +80,37 @@ class LTGraph{
 
         return counter;
     }
+    static bool node_comparator(node_info n1, node_info n2){
+        return n1.degree < n2.degree;
+    }
+
+    void greedy(){
+        int V = G.size();
+        vector<node_info> degree(V);
+
+        for(int i = 0; i < V; ++i) {
+            degree[i].node = i;
+            degree[i].degree = G[i].size();
+        }
+        sort(degree.begin(),degree.end(), node_comparator);
+        bool stop = false;
+        int e_degree = 1;
+        vector<int> old_S;
+        while(!stop){
+            vector<int> aux;
+            for(int j = 0; j < V; ++j)if(degree[j].degree > e_degree) aux.push_back(degree[j].node);
+            old_S = S;
+            S = aux;
+            if(procedure() != V) stop = true;
+        }
+        cout << "MINIMUM INITIAL NODES: {";
+        int old_S_size = old_S.size();
+        for(int k = 0; k < old_S_size; ++k) cout << old_S[k] << ", ";
+        cout << "}" << endl;
+    }
+
+
+
 
 };
 
--- main.cc
+++ main.cc
@@ -7,14 +7,18 @@ using namespace std;
 
 int main(){
     vector<vector<int> > G = { {1}, {0, 2}, {1, 3}, {2, 4, 5}, {3, 6}, {3, 7}, {4, 8}, {5, 9}, {6}, {7} };
-    double p = 0.8;
+    //double p = 0.8;
     double r = 0.4;
-    vector<int> S = {0, 7, 8};
 
+    /* PROCEDURE TESTING
+    vector<int> S = {0, 7, 8};
     LTGraph LT( G, S, r);
     ICGraph IC(G, S, p);
     cout << "With an LT procedure there are " << LT.procedure() << " node(s) active(s) with a R of " << r << endl;
     cout << endl;
     cout << "With an IC procedure there are " << IC.procedure() << " node(s) active(s) with a P of " << p << endl;
+     */
+    LTGraph LT(G,r);
+    LT.greedy();
 }
 
