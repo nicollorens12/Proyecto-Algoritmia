01cebcfa4c3cdbd3daa86518a186b4971cbb87b7
nicollorens12 <nickskate23@gmail.com>
2023-04-10 22:38:30 +0200

    solved compilation errors

--- localSearchIC.cpp
+++ localSearchIC.cpp
@@ -9,20 +9,20 @@
 
 using namespace std;
 
-random_device rd;
-mt19937 gen(rd());
+random_device rd_ic;
+mt19937 gen_ic(rd_ic());
 
-int rand_node(int V) {
+int rand_node_ic(int V) {
     uniform_int_distribution<> distribucion(0, V - 1);
-    return distribucion(gen);
+    return distribucion(gen_ic);
 }
 
-vector<int> random_set(int V, double p) {
+vector<int> random_set_ic(int V, double p) {
     vector<int> aux;
     uniform_real_distribution<double> dis(0.0, 1.0);
 
     for (int i = 0; i < V; i++) {
-        double rand = dis(gen);
+        double rand = dis(gen_ic);
 
         if (rand > p) {
             aux.push_back(i);
@@ -31,28 +31,28 @@ vector<int> random_set(int V, double p) {
     return aux;
 }
 
-int sol_score(vector<int> sol, int V) {
+int sol_score_ic(vector<int> sol, int V) {
     int sol_size = sol.size();
     return (100 / V) * (V - sol_size);
 }
 
-vector<int> generate_neighbor(vector<int> sol, vector<bool>& active, int V) {
-    int random_number = rand_node(V);
+vector<int> generate_neighbor_ic(vector<int> sol, vector<bool>& active, int V) {
+    int random_number = rand_node_ic(V);
     while (!active[random_number]) {
-        random_number = rand_node(V);
+        random_number = rand_node_ic(V);
     }
     active[random_number] = false;
     sol.erase(remove(sol.begin(), sol.end(), random_number), sol.end());
     return sol;
 }
 
-bool its_valid(const vector<vector<int> >& G, const vector<int>& sol, double p) {
+bool its_valid_ic(const vector<vector<int> >& G, const vector<int>& sol, double p) {
     int V = G.size();
     vector<int> S(sol);
     return simulate_IC(G, S, p) == V;
 }
 
-bool more_degree(const vector<int>& S, const vector<int>& neighbor, const vector<vector<int> >& G) {
+bool more_degree_ic(const vector<int>& S, const vector<int>& neighbor, const vector<vector<int> >& G) {
     int s_counter = 0;
     int neighbor_counter = 0;
     for (int i : S) {
@@ -64,7 +64,7 @@ bool more_degree(const vector<int>& S, const vector<int>& neighbor, const vector
     return neighbor_counter > s_counter;
 }
 
-bool is_updated(const vector<int>& S, const vector<int>& new_S) {
+bool is_updated_ic(const vector<int>& S, const vector<int>& new_S) {
     return !equal(S.begin(), S.end(), new_S.begin());
 }
 
@@ -74,11 +74,11 @@ vector<int> local_searchIC(vector<vector<int> > G, double p) {
     double cooling_rate = 0.99;
     int V = G.size();
 
-    vector<int> rand_set = random_set(V, p);
+    vector<int> rand_set = random_set_ic(V, p);
     vector<bool> active(V, false);
 
-    while (rand_set.empty() || !its_valid(G, rand_set, p)) {
-        rand_set = random_set(V, p);
+    while (rand_set.empty() || !its_valid_ic(G, rand_set, p)) {
+        rand_set = random_set_ic(V, p);
     }
 
     vector<int> S = rand_set;
@@ -86,15 +86,15 @@ vector<int> local_searchIC(vector<vector<int> > G, double p) {
     for (int i : S) {
         active[i] = true;
     }
-    int quality = sol_score(S, V);
+    int quality = sol_score_ic(S, V);
     int iteration = 0;
     double temperature = initial_temperature;
 
     while (iteration < max_iterations && quality < V) {
-        vector<int> neighbor_S = generate_neighbor(S, active, V);
-        int neighbor_quality = sol_score(neighbor_S, V);
+        vector<int> neighbor_S = generate_neighbor_ic(S, active, V);
+        int neighbor_quality = sol_score_ic(neighbor_S, V);
 
-        if (its_valid(G, neighbor_S, p) && (neighbor_quality > quality || (neighbor_S.size() == S.size() && more_degree(S, neighbor_S, G)))) {
+        if (its_valid_ic(G, neighbor_S, p) && (neighbor_quality > quality || (neighbor_S.size() == S.size() && more_degree_ic(S, neighbor_S, G)))) {
             S = neighbor_S;
             quality = neighbor_quality;
         }   else {
@@ -109,7 +109,7 @@ vector<int> local_searchIC(vector<vector<int> > G, double p) {
         iteration++;
 
         // Break the loop if the solution hasn't been updated for a certain number of iterations
-        if (!is_updated(S, neighbor_S)) {
+        if (!is_updated_ic(S, neighbor_S)) {
             break;
         }
     }
